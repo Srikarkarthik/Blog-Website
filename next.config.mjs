c45212143b7bc8cd3f006c8002bed169491d7282
Srikarkarthik <srikarkarthik93@gmail.com>
2025-07-25 20:12:25 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['res.cloudinary.com'], // ✅ Allow images from Cloudinary
+  },
+};
 
 export default nextConfig;
